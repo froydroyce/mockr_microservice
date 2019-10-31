27aca0d0b668e65de5bfcc7b2b8dd9bc438953b7
Sejin Kim <froydroyce@gmail.com>
2019-10-30 20:32:25 -0600

    Implement CORS

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var interviewsRouter = require('./routes/interviews');
+const indexRouter = require('./routes/index');
+const interviewsRouter = require('./routes/interviews');
 
-var app = express();
+const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
